0b71479a6f75acb471ea701600736ae30fb82beb
Pranav Shinde <pranavshinde6773@gmail.com>
2021-05-08 20:58:24 +0530

    EX 5

---
+++ strings/pr_05.py
@@ -0,0 +1,6 @@
+#write a program to detect double spaces in a string
+letter = "Dear Harry, This Python course is nice! Thanks!"
+print(letter)
+
+formatted_letter = "Dear Harry,\n\tThis Python course is nice!\nThanks!"
+print(formatted_letter)
