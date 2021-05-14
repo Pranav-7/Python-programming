da702e326a76038c131e640c902474074d8aa4b1
Pranav Shinde <pranavshinde6773@gmail.com>
2021-05-15 00:05:05 +0530

    loops

---
+++ loops/pr.py
@@ -0,0 +1,44 @@
+n=3
+
+for i in range(1,2):
+    print("*" * (2*i+1), end="")
+    break
+     
+
+print("\n")
+
+for i in range(1,2):
+     print("*" * (i*1), end="")
+     print(" " * (i*1), end="")
+     print("*" * (i*1), end="")
+     break      
+print(end="")
+ 
+for i in range(1,2):
+    print("*" * (2*i+1), end="")
+    break      
+       
+    
+
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+        
+     
