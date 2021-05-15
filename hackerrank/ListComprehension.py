23753fd18fad1afcc74258cb05d1c36149593585
Pranav Shinde <pranavshinde6773@gmail.com>
2021-05-15 22:52:11 +0530

    hackerrank badge solution

---
+++ hackerrank/ListComprehension.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    finalList = [];
+    ls = [];
+    for X in range(x+1):
+        for Y in range(y+1):
+            for Z in range(z+1):
+                if X+Y+Z != n:
+                    ls = [X,Y,Z];
+                    finalList.append(ls);
+print(finalList);
